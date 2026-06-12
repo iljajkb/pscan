5e238e8726f10ca6c1be400ec1d24b603c87c184
iljajkb <ilja.jakob@gmail.com>
2026-06-12 13:51:56 +0200

    feat: simple 3-way port scan of google

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::net::TcpStream;
+use std::time::Duration;
+use std::net::ToSocketAddrs;
+
+
 fn main() {
-    println!("Hello, world!");
+  let host = "google.com";
+  let port = "443";
+
+
+  let timeout = Duration::from_secs(3);
+  let target = format!("{}:{}", host, port);
+  println!("Target: {}:{}",host, port);
+
+  match target.to_socket_addrs() {
+    Ok(mut addrs) => {
+      if let Some(socket_addr) = addrs.next() {
+        match TcpStream::connect_timeout(&socket_addr, timeout) {
+          Ok(_) => println!("{}/tcp offen", port),
+          Err(_) => println!("{}/tcp geschlossen", port),
+        }
+      } else {
+        println!("Error: could not find IP for given domain");
+      }
+    }
+    Err(_e) => println!("DNS error!")
+  }
 }
